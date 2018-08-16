238e43228baffc0c5ceeefa181c2a5ebb429417e
kassanj <kassandra@whereby.us>
2018-08-15 20:54:15 -0700

    move components into separate files

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    );
+class App extends React.Component {
+
+  componentDidMount () {
+     const { dispatch } = this.props
+     dispatch(handleInitialData())
   }
+
+  // Subscribe -> Adds a change listener.
+  // It will be called any time an action is dispatched,
+  // and some part of the state tree may potentially have changed.
+   render() {
+
+     if (loading === true) {
+       return <h3>Loading...</h3>
+     }
+
+     return (
+        <div>
+          <ConnectedTodos />
+          <ConnectedGoals />
+        </div>
+     )
+   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App)
